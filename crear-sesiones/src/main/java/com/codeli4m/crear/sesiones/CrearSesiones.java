ea13499b977891919aedfd160fc742c9e05a66d9
li4m <applecdos504@gmail.com>
2022-09-03 23:30:55 -0500

    crear sesiones ucv tools desktop app v1.0.0.0

---
+++ crear-sesiones/src/main/java/com/codeli4m/crear/sesiones/CrearSesiones.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.codeli4m.crear.sesiones;
+
+/**
+ *
+ * @author HP
+ */
+public class CrearSesiones {
+
+    public static void main(String[] args) {
+        crearSesionesGUI cal = new crearSesionesGUI();
+        cal.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
---
+++ crearSesiones/src/main/java/com/codeli4m/crearsesiones/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.codeli4m.crearsesiones;
+
+/**
+ *
+ * @author HP
+ */
+//Main-Class: com.codeli4m.crearsesiones.Main
+public class Main {
+
+    public static void main(String[] args) {
+        crearSesionesGUI mainGUI = new crearSesionesGUI();
+        mainGUI.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
